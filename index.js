9066729f501f746caa84f9bc08d9a2cfc0eddaac
uditvegad <83854907+buddyisOP@users.noreply.github.com>
2021-09-10 08:20:49 +0000

    updated v13 help command with select menu

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const config = require("dot-env")
 const { Client, Intents, Collection } = require("discord.js"),
 { token, prefix, color, ownerId } = require("./settings.json"),
 client = new Client( { intents: [ Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES ] })
@@ -8,4 +9,4 @@ client.aliases = new Collection();
 client.settings = { prefix, color, ownerId }
 
 for(let handler of  ["slash_command", "prefix_command", "event"]) require(`./handler/${handler}`)(client);
-client.login(token)
+client.login(process.env.token)
